981daf88cca8f2317cdfc40945d097b6cc4bc42b
Alberto <albertiae.digital@gmail.com>
2023-01-07 11:20:30 -0400

    Created class App

---
+++ js/app.js
@@ -0,0 +1,9 @@
+class App {
+	constructor(downloads, rating, storageSpace) {
+		this.downloads = downloads;
+		this.rating = rating;
+		this.storageSpace = storageSpace;
+		this.open = false;
+		this.installed = false;
+	}
+}
