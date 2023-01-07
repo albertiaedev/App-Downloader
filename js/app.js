abde33bf8dfd8dce1e521136b26b8b1dfb9542bc
Alberto <albertiae.digital@gmail.com>
2023-01-07 11:43:37 -0400

    Created a function to open the app

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 class App {
-	constructor(downloads, rating, storageSpace) {
+	constructor(name, downloads, rating, storageSpace) {
+		this.name = name;
 		this.downloads = downloads;
 		this.rating = rating;
 		this.storageSpace = storageSpace;
 		this.open = false;
 		this.installed = false;
+	}
+	openApp() {
+		if (this.open === false) {
+			this.open === true;
+			document.write(`${this.name} has been opened succesfully.`)
+		}
 	}
 }
